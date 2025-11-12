2ae2d68b64ceff0cc3b58e8c32331f2e8ec93deb
Parth Patil <parthvin@gmail.com>
2025-11-12 02:42:36 -0500

    Removed old dataloader

--- dataloader/__init__.py
+++ dataloader/__init__.py
@@ -1,28 +1,8 @@
 """Dataloader utilities for training tasks."""
 
-from .combined_similarity_dataloader import (
-    CombinedSimilarityBatch,
-    build_combined_similarity_loader,
-)
+from .combined_similarity_dataloader import (CombinedSimilarityBatch,
+                                             build_combined_similarity_loader)
 from .imagenet_dataset import (ClassBalancedBatchSampler,
                                GenericBalancedDataLoader, GenericImageDataset,
                                ImageNetBalancedDataLoader, ImageNetDataset)
 from .model_npz_dataset import ModelParameterDataset
-from .similarity_datasets import (DatasetTokenDataset, ModelEmbeddingDataset,
-                                  build_dataset_token_loader,
-                                  build_model_embedding_loader)
-
-__all__ = [
-    "ModelParameterDataset",
-    "GenericImageDataset",
-    "GenericBalancedDataLoader",
-    "ImageNetDataset",
-    "ImageNetBalancedDataLoader",
-    "ClassBalancedBatchSampler",
-    "ModelEmbeddingDataset",
-    "DatasetTokenDataset",
-    "build_model_embedding_loader",
-    "build_dataset_token_loader",
-    "CombinedSimilarityBatch",
-    "build_combined_similarity_loader",
-]
